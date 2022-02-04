7f702c5178159cb12200f31fbd38fafdabdc7e0d
deleon06 <gabrieldeleon-ny@hotmail.com>
2022-02-04 12:49:52 -0500

    adding some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'articles'},
+  { path: '**', redirectTo: 'articles'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
